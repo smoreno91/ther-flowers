1f61daa8bb6ea77d4d2bb95f29fbde2c1667702f
Santiago Moreno <smoreno91@gmail.com>
2016-12-16 01:41:28 +0000

    Change personal email

--- assets/php/send.php
+++ assets/php/send.php
@@ -1,6 +1,6 @@
 <?php
 	
-	$destination = "ramifloreslaceja@hotmail.com"; // email address of destination
+	$destination = "smoreno91@gmail.com"; // email address of destination
 	
 	error_reporting (E_ALL ^ E_NOTICE);
 
